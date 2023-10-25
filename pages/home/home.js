8fe05cf18a20211f2099dfaff48ce88cced22cb9
AchoDev <95191621+AchoDev@users.noreply.github.com>
2023-10-25 17:28:59 +0200

    camera controls + cleanup

--- pages/home/home.js
+++ pages/home/home.js
@@ -13,6 +13,7 @@ document.getElementById("home").appendChild(renderer.domElement);
 const geometry = new THREE.BoxGeometry(1, 1, 0.2);
 const material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
 export const cube = new THREE.Mesh(geometry, material);
+cube.visible = false;
 const loader = new GLTFLoader();
 loader.load('room.glb', function (gltf) {
     gltf.scene.traverse(function (node) {
@@ -31,13 +32,13 @@ loader.load('room.glb', function (gltf) {
 // lights
 // '#9A5FC0'
 const ambient = new THREE.AmbientLight(0xFFFFFF, 0.5);
-const light = new THREE.SpotLight('#FFFFFF', 35, 100, 200, 0.25);
+const light = new THREE.SpotLight(0xffffd9, 100, 100, 250, 0.25);
 light.shadow.bias = -0.0005;
 light.castShadow = true;
 const monitorlight1 = new THREE.RectAreaLight('#FFFFFF', 1, 10, 5);
 const monitorlight2 = new THREE.RectAreaLight('#FFFFFF', 1, 10, 5);
 function setInitialTransforms() {
-    light.position.set(0, 4.67, 0);
+    light.position.set(0, 5.67, -0.5);
     light.rotation.set(8, 3.1, 0);
     monitorlight1.position.set(0.3, 2.3, -1.8);
     monitorlight1.rotation.set(0, 3, 0);
@@ -56,12 +57,53 @@ function addObjectsToScene() {
 }
 setInitialTransforms();
 addObjectsToScene();
+let originalCamX = 0;
+let originalCamY = 3;
+let originalCamZ = 3;
 let mouseclicked = false;
+let buttonPressed = false;
+const buttonMovement = [
+    [0, 4],
+    [-1.5, 3],
+    [0, 3],
+    [1.5, 3],
+    [0, 2],
+    [0, 3],
+    [0, 3],
+];
+const buttonMovementZ = [
+    null,
+    null,
+    3,
+    null,
+    null,
+    1.5,
+    4.5
+];
+Array.from(document.getElementById("movement").children).forEach((button, index) => {
+    button.onmousedown = (e) => {
+        buttonPressed = true;
+        if (buttonMovementZ[index] != null) {
+            originalCamZ = buttonMovementZ[index];
+            if (buttonMovementZ[index] !== 3)
+                return;
+        }
+        originalCamX = buttonMovement[index][0];
+        originalCamY = buttonMovement[index][1];
+    };
+    button.onmouseup = (e) => {
+        buttonPressed = false;
+    };
+});
 function animate() {
     requestAnimationFrame(animate);
-    if (!mouseclicked) {
+    if (!mouseclicked || buttonPressed) {
         camera.rotation.x = camera.rotation.x / 1.05;
         camera.rotation.y = camera.rotation.y / 1.05;
+        // camera.position.x = camera.position.x / 1.05
+        camera.position.x = ((camera.position.x - originalCamX) / 1.05) + originalCamX;
+        camera.position.y = ((camera.position.y - originalCamY) / 1.05) + originalCamY;
+        camera.position.z = ((camera.position.z - originalCamZ) / 1.05) + originalCamZ;
     }
     renderer.render(scene, camera);
 }
@@ -77,7 +119,9 @@ const smallMoveAmount = 0.0001;
 window.addEventListener('mousemove', (e) => {
     if (!mouseclicked) {
         camera.rotation.y -= e.movementX * smallMoveAmount;
-        camera.rotation.x -= e.movementY * smallMoveAmount;
+        camera.rotation.x -= e.movementY * smallMoveAmount * 2;
+        camera.position.x -= e.movementX * smallMoveAmount * 5;
+        camera.position.y += e.movementY * smallMoveAmount * 5;
         return;
     }
     camera.rotation.y += e.movementX * moveAmount;
--- pages/home/home.ts
+++ pages/home/home.ts
@@ -24,6 +24,7 @@ const geometry: THREE.BoxGeometry = new THREE.BoxGeometry(1, 1, 0.2)
 const material: THREE.MeshLambertMaterial = new THREE.MeshLambertMaterial({color: 0x00ff00})
 export const cube: THREE.Mesh = new THREE.Mesh(geometry, material)
 
+cube.visible = false;
 
 const loader = new GLTFLoader()
 
@@ -55,8 +56,9 @@ loader.load(
 
 // lights
 // '#9A5FC0'
+
 const ambient = new THREE.AmbientLight(0xFFFFFF, 0.5)
-const light = new THREE.SpotLight('#FFFFFF', 35, 100, 200, 0.25)
+const light = new THREE.SpotLight(0xffffd9, 100, 100, 250, 0.25)
 light.shadow.bias = -0.0005
 light.castShadow = true
 
@@ -68,7 +70,7 @@ const monitorlight2 = new THREE.RectAreaLight('#FFFFFF', 1, 10, 5)
 
 function setInitialTransforms() {
 
-  light.position.set(0, 4.67, 0)
+  light.position.set(0, 5.67, -0.5)
   light.rotation.set(8, 3.1, 0)
 
   monitorlight1.position.set(0.3, 2.3, -1.8)
@@ -94,15 +96,66 @@ function addObjectsToScene() {
 setInitialTransforms()
 addObjectsToScene()
 
+let originalCamX = 0;
+let originalCamY = 3;
+let originalCamZ = 3;
+
 let mouseclicked: boolean = false
+let buttonPressed: boolean = false
+
+const buttonMovement = [
+  [0, 4],
+  [-1.5, 3],
+  [0, 3],
+  [1.5, 3],
+  [0, 2],
+  [0, 3],
+  [0, 3],
+]
+
+const buttonMovementZ = [
+  null,
+  null,
+  3,
+  null,
+  null,
+  1.5,
+  4.5
+]
+
+Array.from(document.getElementById("movement").children).forEach((button: HTMLButtonElement, index) => {
+  button.onmousedown = (e) => {
+
+    buttonPressed = true
+    if(buttonMovementZ[index] != null) {
+      originalCamZ = buttonMovementZ[index]
+      
+      if(buttonMovementZ[index] !== 3) return
+    }
+
+    originalCamX = buttonMovement[index][0]
+    originalCamY = buttonMovement[index][1]
+
+  }
+
+  button.onmouseup = (e) => {
+    buttonPressed = false
+  }
+})
 
 function animate() {
 	requestAnimationFrame( animate );
 
-  if(!mouseclicked) {
+  if(!mouseclicked || buttonPressed) {
     camera.rotation.x = camera.rotation.x / 1.05
     camera.rotation.y = camera.rotation.y / 1.05
+    // camera.position.x = camera.position.x / 1.05
+
+    camera.position.x = (( camera.position.x - originalCamX ) / 1.05 ) + originalCamX
+    camera.position.y = (( camera.position.y - originalCamY ) / 1.05 ) + originalCamY
+    camera.position.z = (( camera.position.z - originalCamZ ) / 1.05 ) + originalCamZ
   }
+
 
 	renderer.render( scene, camera );
 }
@@ -122,10 +175,14 @@ const smallMoveAmount: number = 0.0001
 window.addEventListener('mousemove', (e) => {
   if(!mouseclicked) {
     camera.rotation.y -= e.movementX * smallMoveAmount
-    camera.rotation.x -= e.movementY * smallMoveAmount
+    camera.rotation.x -= e.movementY * smallMoveAmount * 2
+
+    camera.position.x -= e.movementX * smallMoveAmount * 5
+    camera.position.y += e.movementY * smallMoveAmount * 5
+    
     return
   }
-
+  
   camera.rotation.y += e.movementX * moveAmount
   camera.rotation.x += e.movementY * moveAmount
 })
